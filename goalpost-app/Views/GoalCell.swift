16ef0c9005621dfe0b24362c6fbf0adb8233126d
Chris Parr <parr.cj@gmail.com>
2019-03-02 20:44:56 +0000

    Building Goal Cell

---
+++ goalpost-app/Views/GoalCell.swift
@@ -0,0 +1,24 @@
+//
+//  GoalCell.swift
+//  goalpost-app
+//
+//  Created by Christopher Parr on 02/03/2019.
+//  Copyright © 2019 Christopher Parr. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
